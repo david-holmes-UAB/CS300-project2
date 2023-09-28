e98da973f5185f9c4b17f0c8cd107e0fd8b8fdac
Ruler (David) <azurecode516@gmail.com>
2023-09-28 18:10:13 -0500

    implemented moveForward()

--- shapelist.cpp
+++ shapelist.cpp
@@ -35,22 +35,66 @@ void ShapeList::moveToFront(Shape *sp) {
 }
 
 void ShapeList::moveToBack(Shape *sp) {
+    int pos = 0;
     int found = 0;
     for (Shape *s : ShapeList()) {
         if (s == sp) {
             found = 1;
+            break;
         }
+        pos++;
     }
 
     if (found == 1) {
-
+        Shape * temp = sp;
+        ShapeList().removeAt(pos);
+        ShapeList().push_back(sp);
     }
     else {
         std::cout << "Error: the input shape was not found in the list.\n\n";
     }
 }
 
+void ShapeList::moveForward(Shape *sp) {
+    int pos = 0;
+    int found = 0;
+    for (Shape *s : ShapeList()) {
+        if (s == sp) {
+            found = 1;
+            break;
+        }
+        pos++;
+    }
 
+    if (found == 1) {
+        if (ShapeList().get(0) != sp) {
+            Shape * temp = sp;
+            Shape * old = ShapeList().get(pos - 1);
+            ShapeList().removeAt(pos);
+            ShapeList().insertAt(pos - 1, sp); /* Swaps the two items - 
+                                           *  moves the object in the position it was pushed
+                                           *  to, to the original position of sp */
+            ShapeList().insertAt(pos, old);
+        } 
+        else {
+            std::cout << "The inputted shape can't be moved any further forward.\n\n";
+        }
+    }
+    else {
+        std::cout << "Error: the input shape was not found in the list.\n\n";
+    }
+
+}
+
+void ShapeList::moveBackward(Shape *sp) {
+
+}
+
+Shape * getShapeAt(double x, double y) {
+    Shape *sp = nullptr;
+
+    return sp;
+}
 
 
 void ShapeList::draw(GWindow & gw) const{
