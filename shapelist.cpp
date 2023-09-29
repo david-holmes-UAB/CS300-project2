192955cb4b8ff2bc36597e1cbbedea671a20da4c
Ruler (David) <azurecode516@gmail.com>
2023-09-28 19:17:56 -0500

    implemented moveBackward() and getShapeAt()

--- shapelist.cpp
+++ shapelist.cpp
@@ -58,6 +58,8 @@ void ShapeList::moveToBack(Shape *sp) {
 void ShapeList::moveForward(Shape *sp) {
     int pos = 0;
     int found = 0;
+    // is there a more reliable way to locate a shape within the 
+    // vector? Found no helpful implementations in the stanford library
     for (Shape *s : ShapeList()) {
         if (s == sp) {
             found = 1;
@@ -87,18 +89,53 @@ void ShapeList::moveForward(Shape *sp) {
 }
 
 void ShapeList::moveBackward(Shape *sp) {
+    int pos = 0;
+    int found = 0;
+    for (Shape *s : ShapeList()) {
+        if (s == sp) {
+            found = 1;
+            break;
+        }
+        pos++;
+    }
+
+    if (found == 1) {
+        int end = ShapeList().size() - 1;
+        if (ShapeList().get(end) != sp) {
+            Shape * temp = sp;
+            Shape * old = ShapeList().get(pos + 1);
+            ShapeList().removeAt(pos);
+            ShapeList().insertAt(pos + 1, sp);
+            ShapeList().insertAt(pos, old);
+        }
+        else {
+            std::cout << "The inputted shape cannot be moved any further back.";
+        }
+
+    }
+    else {
+        std::cout << "Error: the input shape was not found in the list.\n\n";
+    }
 
 }
 
 Shape * getShapeAt(double x, double y) {
-    Shape *sp = nullptr;
+    Shape * sp = nullptr;
+    for (Shape * s : ShapeList()) {
+        if (s->contains(x, y)) {
+            sp = s;
+            break;
+        }
+    }
 
     return sp;
 }
 
 
 void ShapeList::draw(GWindow & gw) const{
-    for (Shape *sp : ShapeList()) {
-        sp->draw(gw);
+    // Draws starting from the back of the vector
+    int i = ShapeList().size() - 1;
+    for (i >= 0; i--;) {
+        ShapeList().get(i)->draw(gw);
     }
 }
